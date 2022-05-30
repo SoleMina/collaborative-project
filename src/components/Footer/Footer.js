29326af4877b9ec29cbb1503301dfded7fda411d
JhonatanPG02 <jhonatan.prado.gutierrez@gmail.com>
2022-05-30 17:47:04 -0500

    Footer change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>&copy;2022 Todos los derechos reservados</h1>
+      <h1>Todos los derechos reservados-&copy;2022 </h1>
     </div>
   );
 };
