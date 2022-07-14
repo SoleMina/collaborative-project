a094c7e668e1190fbc4493018090da211afea3d0
JhonatanPG02 <jhonatan.prado.gutierrez@gmail.com>
2022-07-14 16:15:33 -0500

    feat(component/Header): add button search
    Se agrego un buscador.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,11 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header__container">
-      <h1>Header</h1>
+      <h1>PROYECTO</h1>
+      <div className="search">
+        <input type='text' className="input"/>
+        <button className="input">Buscar</button>
+      </div>
     </div>
   );
 };
